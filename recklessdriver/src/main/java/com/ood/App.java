44f9cb041cc15d9b4c88b138670dabc06b6564cc
Avinash Kumar <officialaviid@gmail.com>
2024-12-29 19:15:15 +0000

    starting implementation

--- recklessdriver/src/main/java/com/ood/App.java
+++ recklessdriver/src/main/java/com/ood/App.java
@@ -6,13 +6,10 @@ package com.ood;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        FireHydrant fireHydrant = new FireHydrant();
-        fireHydrant.setDamage(100);
-        fireHydrant.setName("FireHydrant");
-
-
+        
     }
 }
