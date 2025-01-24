3d8902f78ea5c8b96f3e3660bb547568706c6785
Avinash <officialaviid@gmail.com>
2025-01-24 20:04:47 +0000

    aop added

--- springaop/src/main/java/com/example/demo/AuthenticationAspect.java
+++ springaop/src/main/java/com/example/demo/AuthenticationAspect.java
@@ -11,15 +11,15 @@ public class AuthenticationAspect {
 
     @Pointcut("within(com.example.demo..*)")
     public void authenticationPointCut(){
-
+        System.out.println("Withing authentication point cut");
     }
 
     @Pointcut("within(com.example.demo..*)")
     public void authorizationPointCut(){
-
+        System.out.println("Withing authorization point cut");
     }
 
-    @Before("authenticatingPointCut() && authorizationPointCut()")
+    @Before("authenticatingPointCut() || authorizationPointCut()")
     public void authenticate(){
         System.out.println("Authenticating...");
     }
--- springaop/src/main/java/com/example/demo/ShoppingCart.java
+++ springaop/src/main/java/com/example/demo/ShoppingCart.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ShoppingCart {
     
-    public void checkout(){
+    public void checkout(String hello){
         System.out.println("Checkout method from shopping cart called");
     }
 }
