f9c35f1fb7d6c38dd6f3258b5570d568fb44ffcd
kavinash-samsung <officialaviid@gmail.com>
2025-01-26 00:36:22 +0530

    connected postgres with application

--- springboot/src/main/java/com/example/springboot/repository/DepartmentRepository.java
+++ springboot/src/main/java/com/example/springboot/repository/DepartmentRepository.java
@@ -1,7 +1,6 @@
 package com.example.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.springboot.entity.Department;
